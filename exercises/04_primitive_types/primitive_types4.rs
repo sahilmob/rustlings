87ab37d32ed4005c1ca842ac0ebe44d328d43741
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2024-12-29 18:39:46 +0300

    primitives

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -4,12 +4,14 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use std::ops::Index;
+
     #[test]
     fn slice_out_of_array() {
         let a = [1, 2, 3, 4, 5];
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
-        // let nice_slice = ???
+        let nice_slice = &a[1..4];
 
         assert_eq!([2, 3, 4], nice_slice);
     }
